a3f73ef942d5d6ff047a584fa23aa1e9d6de8f4d
rajbahadur <raj02.akg@gmail.com>
2020-07-11 16:20:42 +0530

    first commit

---
+++ src/main/java/spring_boot_thymeleaf/spring_boot_hello_world/WelcomeController.java
@@ -0,0 +1,39 @@
+package spring_boot_thymeleaf.spring_boot_hello_world;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class WelcomeController {
+
+    // inject via application.properties
+    @Value("${welcome.message}")
+    private String message;
+
+    private List<String> tasks = Arrays.asList("Spring Boot", "Java8", "Hibernate", "Jenkins", "AWS Cloud", "DevOps", "Design Pattern");
+
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("message", message);
+        model.addAttribute("tasks", tasks);
+
+        return "welcome"; //view
+    }
+
+    // /hello?name=kotlin
+    @GetMapping("/hello")
+    public String mainWithParam(
+            @RequestParam(name = "name", required = false, defaultValue = "")
+			String name, Model model) {
+
+        model.addAttribute("message", name);
+
+        return "welcome"; //view
+    }
+
+}
